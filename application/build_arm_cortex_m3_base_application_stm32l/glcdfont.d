960908e8179d773b2f77339d3d99ba9dc33d04a5
Lam Hoang <lamht1998@gmail.com>
2019-08-07 14:59:05 +0700

    Delete extra files

--- application/build_arm_cortex_m3_base_application_stm32l/glcdfont.d
+++
@@ -1,2 +0,0 @@
-build_arm_cortex_m3_base_application_stm32l/glcdfont.o: \
- sources/driver/Adafruit_ssd1306syp/glcdfont.cpp
--- application/build_arm_cortex_m3_base_application_stm32l/sys_ctrl.d
+++
@@ -1,2 +0,0 @@
-build_arm_cortex_m3_base_application_stm32l/sys_ctrl.o: \
- sources/platform/stm32l/sys_ctrl.s
